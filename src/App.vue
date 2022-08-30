8a759fa2dc44deefd04387c072d690c65d30d937
Mohammadali <m.erfani@communiacs.de>
2022-08-30 14:21:38 +0200

    check for dev

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <h2>Check for github</h2>
+      <h2>Check for dev branch</h2>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
