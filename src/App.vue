3c6022a6dbb7c9f2a5d650405142dfdc142e2c5c
Mohammadali <m.erfani@communiacs.de>
2022-08-30 14:19:12 +0200

    check for github

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h2>Check for github</h2>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
